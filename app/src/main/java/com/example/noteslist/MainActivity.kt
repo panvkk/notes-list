5cc42ff2627c54eaaaaba684537722dbeee9480e
panvkk <pnvk1248@gmail.com>
2026-02-24 18:02:25 +0300

    add main activity and change manifest for these changes

--- app/src/main/java/com/example/noteslist/MainActivity.kt
+++ app/src/main/java/com/example/noteslist/MainActivity.kt
@@ -1,3 +1,13 @@
 package com.example.noteslist
 
-//тут будешь ваша активити
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+    }
+}
