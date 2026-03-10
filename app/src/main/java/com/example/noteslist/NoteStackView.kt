dab9b45a35a606722f185078e9d19621fbd04ea9
panvkk <pnvk1248@gmail.com>
2026-03-11 00:27:02 +0300

    add part of logic responsible for expand and collapse of stack

--- app/src/main/java/com/example/noteslist/NoteStackView.kt
+++ app/src/main/java/com/example/noteslist/NoteStackView.kt
@@ -1,12 +1,16 @@
 package com.example.noteslist
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Point
+import android.graphics.Rect
 import android.util.AttributeSet
 import android.util.Log
 import android.view.ViewGroup
+import androidx.core.graphics.withTranslation
 import androidx.core.view.isGone
-import androidx.core.view.marginLeft
-import androidx.core.view.marginRight
 import com.example.noteslist.core.toLocalDate
 
 class NoteStackView @JvmOverloads constructor(
@@ -17,23 +21,65 @@ class NoteStackView @JvmOverloads constructor(
 ) : ViewGroup(context, attrs, defStyleAttr, defStyleRes) {
     private var defaultStackSpacing = 20f
     private var defaultStackMaxVisible = 3
+    private var defaultCollapseButtonHeight = 20f
+    private var defaultVerticalPadding = 20f
+    private var defaultCollapseButtonColor = Color.GRAY
+    private var defaultCollapseButtonSize = 20f
+    private var defaultCollapseButtonText = ""
 
     private var stackSpacing = 20f
     private var stackMaxVisible = 3
+    private var verticalPadding = defaultVerticalPadding
+    private var collapseButtonHeight = defaultCollapseButtonHeight
+    private var collapseButtonSize = defaultCollapseButtonSize
+    private var collapseButtonColor = defaultCollapseButtonColor
+    private var collapseButtonText = defaultCollapseButtonText
     private var _isExpanded = false
     var isExpanded: Boolean
         get() = _isExpanded
         set(value) {
             _isExpanded = value
             requestLayout()
+            if(value) updateGeometry()
             invalidate()
         }
+
+    // Paint
+    private var collapseButtonPaint = Paint().apply { isAntiAlias = true }
+
+    // Геометрия
+    private var collapseButtonRect = Rect()
+    private var collapseButtonPoint = Point()
 
     private var childIndexes: MutableList<Int>? = null
 
 
     init {
+        val resources = context.resources
+        resources.apply {
+            defaultVerticalPadding = resources.getDimensionPixelSize(R.dimen.note_stack_vertical_padding).toFloat()
+            defaultCollapseButtonHeight = resources.getDimension(R.dimen.note_stack_collapse_button_height)
+            defaultCollapseButtonSize = resources.getDimension(R.dimen.note_stack_collapse_button_text_size)
+            defaultCollapseButtonColor = resources.getColor(R.color.note_stack_collapse_button_color)
+            defaultCollapseButtonText = resources.getString(R.string.note_stack_collapse_button_title)
+        }
+        collapseButtonText = defaultCollapseButtonText
+        collapseButtonColor = defaultCollapseButtonColor
+        collapseButtonSize = defaultCollapseButtonSize
+        verticalPadding = defaultVerticalPadding
+        collapseButtonHeight = defaultCollapseButtonHeight
+
         initAttrs(attrs, defStyleAttr, defStyleRes)
+        initPaints()
+        setWillNotDraw(false)
+    }
+
+    private fun initPaints() {
+        collapseButtonPaint.apply {
+            style = Paint.Style.FILL
+            color = collapseButtonColor
+            textSize = this@NoteStackView.collapseButtonSize
+        }
     }
 
     private fun initAttrs(attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) {
@@ -77,8 +123,10 @@ class NoteStackView @JvmOverloads constructor(
 
                     totalHeight += totalChildHeight
                 }
+                totalHeight += (collapseButtonHeight + verticalPadding).toInt()
             }
         }
+
         val measuredWidth = resolveSize(MeasureSpec.getSize(widthMeasureSpec), widthMeasureSpec)
         val measuredHeight = resolveSize(totalHeight, heightMeasureSpec)
         setMeasuredDimension(measuredWidth, measuredHeight)
@@ -89,10 +137,10 @@ class NoteStackView @JvmOverloads constructor(
         var childTop = paddingTop
 
         childIndexes?.let {
-            if(!isExpanded) {
-                for(i in childIndexes) {
+            if (!isExpanded) {
+                for (i in childIndexes) {
                     val child = getChildAt(i)
-                    if(child.isGone) continue
+                    if (child.isGone) continue
 
                     val lp = child.layoutParams as MarginLayoutParams
                     val childWidth = child.measuredWidth - stackSpacing * i
@@ -108,9 +156,9 @@ class NoteStackView @JvmOverloads constructor(
                     childTop += stackSpacing.toInt()
                 }
             } else {
-                for(i in childIndexes) {
+                for (i in childIndexes) {
                     val child = getChildAt(i)
-                    if(child.isGone) continue
+                    if (child.isGone) continue
 
                     val lp = child.layoutParams as MarginLayoutParams
                     val childWidth = child.measuredWidth
@@ -130,6 +178,29 @@ class NoteStackView @JvmOverloads constructor(
         }
     }
 
+    override fun onDraw(canvas: Canvas) {
+        drawCollapseButton(canvas)
+    }
+
+    private fun updateGeometry() {
+        val left = paddingLeft
+        val top = (measuredHeight - collapseButtonHeight).toInt()
+        val right = measuredWidth
+        val bottom = measuredHeight
+        collapseButtonRect.set(left, top, right, bottom)
+        collapseButtonPoint.set(left, (top + collapseButtonHeight / 2).toInt())
+    }
+
+    private fun drawCollapseButton(canvas: Canvas) {
+        canvas.withTranslation(collapseButtonRect.left.toFloat(), collapseButtonRect.top.toFloat()) {
+            this.drawText(
+                collapseButtonText,
+                collapseButtonPoint.x.toFloat(),
+                collapseButtonPoint.y.toFloat(),
+                collapseButtonPaint
+            )
+        }
+    }
     private fun sortIndexes() {
         childIndexes?.let {
             try {
