07bb06bb64cf408dac343d78bcf324e4bb8773db
panvkk <pnvk1248@gmail.com>
2026-03-11 23:41:52 +0300

    fix margins of NoteView inside recyclerView

--- app/src/main/java/com/example/noteslist/ui/recycler/adapters/delegates/NoteDelegate.kt
+++ app/src/main/java/com/example/noteslist/ui/recycler/adapters/delegates/NoteDelegate.kt
@@ -14,7 +14,7 @@ class NoteDelegate : AdapterDelegate<ViewTyped> {
 
     override fun onCreateViewHolder(parent: ViewGroup): RecyclerView.ViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val binding = ItemNoteViewBinding.inflate(inflater)
+        val binding = ItemNoteViewBinding.inflate(inflater, parent, false)
         return NoteViewHolder(binding)
     }
 
