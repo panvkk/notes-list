9e62177897333899dc1e413487a402dc9919ca3e
panvkk <pnvk1248@gmail.com>
2026-04-07 19:18:08 +0300

    finish solution with sliding pane layout, but it seems like I have to rewrite it with jetpack navigation

--- app/src/main/java/com/example/noteslist/presentation/fragment/MainHostFragment.kt
+++ app/src/main/java/com/example/noteslist/presentation/fragment/MainHostFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Lifecycle
@@ -12,6 +13,7 @@ import androidx.lifecycle.repeatOnLifecycle
 import com.example.noteslist.databinding.FragmentMainHostBinding
 import com.example.noteslist.presentation.model.GlobalUiState
 import com.example.noteslist.presentation.viewmodel.GlobalViewModel
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import kotlinx.coroutines.launch
 import kotlin.getValue
 
@@ -33,6 +35,8 @@ class MainHostFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        requireActivity().onBackPressedDispatcher.addCallback(backPressedCallback)
+
         val slidingPane = binding.slidingPaneLayout
         viewLifecycleOwner.lifecycleScope.launch {
             viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
@@ -45,6 +49,42 @@ class MainHostFragment : Fragment() {
                 }
             }
         }
+    }
+
+    private val backPressedCallback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            val slidingPane = binding.slidingPaneLayout
+
+            if(slidingPane.isSlideable && slidingPane.isOpen) {
+                showUnsavedChangesAlert(onConfirm = { slidingPane.closePane() })
+            } else if(slidingPane.isOpen) {
+                showUnsavedChangesAlert(onConfirm = { slidingPane.closePane() })
+                slidingPane.closePane()
+            } else {
+                showOnExitAlert()
+            }
+        }
+    }
+
+    private fun showOnExitAlert() {
+        MaterialAlertDialogBuilder(requireContext())
+            .setTitle("Выйти?")
+            .setMessage("Вы уверены, что хотите выйти из приложения?")
+            .setPositiveButton("Да", { _, _ -> requireActivity().finish() })
+            .setNegativeButton("Нет", null)
+            .show()
+    }
+
+    private fun showUnsavedChangesAlert(onConfirm: () -> Unit) {
+        MaterialAlertDialogBuilder(requireContext())
+            .setTitle("Назад?")
+            .setMessage("Несохранённые данные будут потеряны.")
+            .setPositiveButton("Да", { _, _ ->
+                requireActivity().finish()
+                onConfirm()
+            })
+            .setNegativeButton("Нет",  null)
+            .show()
     }
 
     override fun onDestroyView() {
